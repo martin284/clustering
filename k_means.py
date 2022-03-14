837479b9d1ad111143bcff4a4eabd3d895bb84ea
Martin Brand <m.brand96@gmx.de>
2022-03-14 19:06:02 +0100

    tested algorithm with real data

--- k_means.py
+++ k_means.py
@@ -1,7 +1,6 @@
 import numpy as np
 import sklearn as sk
 from sklearn import datasets # why do I need this?
-import random
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import cdist
 
@@ -9,8 +8,7 @@ def k_means(data, ncluster, n_iteration):
     # choose centroids randomly
     centroid_indices = np.random.choice(len(data), ncluster, replace=False)
     centroids = data[centroid_indices]
-    print(centroids)
-    # start assignment
+    # run the iterations
     for i in range(n_iteration):
         # calculates the euclidean distances for every data point
         distances = cdist(data, centroids, 'euclidean')
@@ -24,16 +22,21 @@ def k_means(data, ncluster, n_iteration):
     return points
 
 if __name__ == "__main__":
-    # iris = sk.datasets.load_iris()
-    # X = iris.data[:, 0:2] # taking only the first 2 dimensions
-    # y = iris.target
-    # plt.scatter(X[:,0], X[:,1], c=y)
+    # # create ndarray with test data
+    # data = np.array([[1, 2], [1, 1], [2, 2], [3, 7], [3, 8], [4, 8], [7, 1],
+    # [7, 2], [8, 2]], dtype=float)
+    # n_cluster = 3
+    # n_iteration = 5
+    # label = k_means(data, n_cluster, n_iteration)
+    # plt.scatter(data[:,0], data[:,1], c=label)
     # plt.show()
 
-    # create ndarray with test data
-    data = np.array([[1, 2], [1, 1], [2, 2], [3, 7], [3, 8], [4, 8], [7, 1],
-    [7, 2], [8, 2]])
+    # now test the algorithm with the iris data
+    iris = sk.datasets.load_iris()
+    X = iris.data[:, 0:2] # taking only the first 2 dimensions
+    y = iris.target
     n_cluster = 3
     n_iteration = 1000
-    points_to_clusters = k_means(data, n_cluster, n_iteration)
-    print(points_to_clusters)
+    label = k_means(X, n_cluster, n_iteration)
+    plt.scatter(X[:,0], X[:,1], c=label)
+    plt.show()
